449a981a655559f16bd70ac8c6bda7bc112fc51d
Abubakirov Vladimir <abubakirov04@mail.ru>
2021-05-30 22:11:59 +0300

    Added entering data to firebase

--- backend.py
+++ backend.py
@@ -1,3 +1,5 @@
+import os.path
+
 import pandas as pd
 import numpy as np
 from difflib import SequenceMatcher
@@ -5,6 +7,8 @@ from translate import Translator
 import ast
 import pathlib
 from dataclasses import dataclass
+import firebase_admin
+from firebase_admin import db
 
 
 @dataclass
@@ -17,9 +21,12 @@ class Game:
 
 
 games = pd.DataFrame()
-processors = pd.DataFrame() # processors and their benchmarks
-videocards = pd.DataFrame() # GPUs and their benchmarks
+processors = pd.DataFrame()  # processors and their benchmarks
+videocards = pd.DataFrame()  # GPUs and their benchmarks
 assemblies = pd.DataFrame()
+
+BASE_DIR = os.path.join(pathlib.Path(__file__).resolve().parent.parent, 'Game-analog-searcher')
+FIREBASE_OBJECT = None
 
 # used for moving games with the most popular developers to the top
 priored_developers = ['Ubisoft', 'Valve', 'CD PROJEKT RED', 'Bungie']
@@ -44,7 +51,6 @@ def __read_all_datasets__():
         assemblies = pd.read_excel(str(pathlib.Path(__file__).parent.absolute()) + '/static/assemblies.xlsx')
 
 
-
 def get_assembly(game_name, given_graphics):
     __read_all_datasets__()
 
@@ -65,13 +71,14 @@ def get_assembly(game_name, given_graphics):
     max_index = -1
     for i in range(len(processor_names)):
         # if we found processor name in game requirements string
-        if ast.literal_eval(game[graphics])['processor'].lower().find(processor_names[i].lower()) != -1: 
+        if ast.literal_eval(game[graphics])['processor'].lower().find(processor_names[i].lower()) != -1:
             max_coincidence = 1.0
             max_index = i
             break
 
         # getting percentage of coincidence
-        tmp = SequenceMatcher(lambda x: x==" ", ast.literal_eval(game[graphics])['processor'].lower(), processor_names[i].lower()).ratio()
+        tmp = SequenceMatcher(lambda x: x == " ", ast.literal_eval(game[graphics])['processor'].lower(),
+                              processor_names[i].lower()).ratio()
         if max_coincidence < tmp:
             max_index = i
             max_coincidence = tmp
@@ -81,34 +88,37 @@ def get_assembly(game_name, given_graphics):
     max_index = -1
     for i in range(len(videocard_names)):
         # if we found videocard name in game requirements string
-        if ast.literal_eval(game[graphics])['graphics'].lower().find(videocard_names[i].lower()) != -1: 
+        if ast.literal_eval(game[graphics])['graphics'].lower().find(videocard_names[i].lower()) != -1:
             max_coincidence = 1.0
             max_index = i
             break
 
         # getting percentage of coincidence
-        tmp = SequenceMatcher(lambda x: x==" ", ast.literal_eval(game[graphics])['graphics'].lower(), videocard_names[i].lower()).ratio()
+        tmp = SequenceMatcher(lambda x: x == " ", ast.literal_eval(game[graphics])['graphics'].lower(),
+                              videocard_names[i].lower()).ratio()
         if max_coincidence < tmp:
             max_index = i
             max_coincidence = tmp
     ideal_videocard = videocard_names[max_index]
 
     # getting benchmark of assembly asked in the requirements
-    ideal_assembly_rating = float(videocards.iloc[list(videocards['Name']).index(ideal_videocard)]['Rating']) + float(processors.iloc[list(processors['name']).index(ideal_processor)]['rating'])
+    ideal_assembly_rating = float(videocards.iloc[list(videocards['Name']).index(ideal_videocard)]['Rating']) + float(
+        processors.iloc[list(processors['name']).index(ideal_processor)]['rating'])
 
     # getting assembly (available at the shop) closest ideal one
     min_difference = max(list(assemblies['rating']))
     ans_assembly_index = -1
     for i in range(len(assemblies)):
         if 0 <= (assemblies.iloc[i]['rating'] - ideal_assembly_rating) < min_difference \
-                and videocards.iloc[list(videocards['Name']).index(assemblies.iloc[i]['graphics'])]['Rating'] > videocards.iloc[list(videocards['Name']).index(ideal_videocard)]['Rating'] \
-                and processors.iloc[list(processors['name']).index(assemblies.iloc[i]['processor'])]['rating'] > processors.iloc[list(processors['name']).index(ideal_processor)]['rating']:
+                and videocards.iloc[list(videocards['Name']).index(assemblies.iloc[i]['graphics'])]['Rating'] > \
+                videocards.iloc[list(videocards['Name']).index(ideal_videocard)]['Rating'] \
+                and processors.iloc[list(processors['name']).index(assemblies.iloc[i]['processor'])]['rating'] > \
+                processors.iloc[list(processors['name']).index(ideal_processor)]['rating']:
             min_difference = assemblies.iloc[i]['rating'] - ideal_assembly_rating
             ans_assembly_index = i
 
     return assemblies.iloc[ans_assembly_index]
 
-    
 
 def game_analog_searcher(income_game):
     """
@@ -117,26 +127,26 @@ def game_analog_searcher(income_game):
     __read_games_dataset__()
 
     # translating game`s name if it was given in Russian
-    # translating cyrillics to latin works worse, trust me)
+    # translating Cyrillic's to latin works worse, trust me)
     translator = Translator(from_lang='ru', to_lang='en')
     game = translator.translate(str(income_game))
 
-    names = np.array(games['name']) # an array containing only game names
-    indexes = dict() # a dict containing game name and its coincidence to a given one
+    names = np.array(games['name'])  # an array containing only game names
+    indexes = dict()  # a dict containing game name and its coincidence to a given one
     for i in range(len(names)):
         indexes[i] = -1.0
     for i in range(len(names)):
         try:
             try:
                 # if game was given right like in steam
-                if names[i].lower().find(game.lower()) != -1: 
+                if names[i].lower().find(game.lower()) != -1:
                     indexes[i] = 1.0
                     continue
             except AttributeError:
                 pass
 
             # getting percentage of coincidence
-            tmp = SequenceMatcher(lambda x: x==" ", game, names[i]).ratio()
+            tmp = SequenceMatcher(lambda x: x == " ", game, names[i]).ratio()
             indexes[i] = tmp
         except TypeError:
             pass
@@ -148,7 +158,8 @@ def game_analog_searcher(income_game):
     feedback_list = list(indexes.keys())[-50::]
     suggested_games = list()
     for i in feedback_list:
-        tmp = Game(str(games.iloc[i]['name']), str(games.iloc[i]['developer']), str(games.iloc[i]['publisher']), str(games.iloc[i]['win_minimum']), str(games.iloc[i]['win_recommended']))
+        tmp = Game(str(games.iloc[i]['name']), str(games.iloc[i]['developer']), str(games.iloc[i]['publisher']),
+                   str(games.iloc[i]['win_minimum']), str(games.iloc[i]['win_recommended']))
         if tmp not in suggested_games:
             suggested_games.append(tmp)
 
@@ -163,3 +174,21 @@ def game_analog_searcher(income_game):
 
     # do not return a slice with more than 10 values!!!
     return game_names[-8::]
+
+
+def init_firebase():
+    global FIREBASE_OBJECT
+    if FIREBASE_OBJECT is None:
+        cred_object = firebase_admin.credentials.Certificate(
+            os.path.join(BASE_DIR, 'firebase_credentials.json'))
+        FIREBASE_OBJECT = firebase_admin.initialize_app(cred_object, {
+            'databaseURL': 'https://computershop-3bc7d-default-rtdb.europe-west1.firebasedatabase.app/'
+        })
+
+
+def make_order(assembly_dict):
+    init_firebase()
+    reference = db.reference('active_orders')
+    assembly_dict['id'] = '2'
+    push = reference.push(assembly_dict)
+    print(len(push.key))
--- chatbot-webhook.py
+++ chatbot-webhook.py
@@ -1,17 +1,17 @@
 from flask import Flask, request, jsonify
 import os
 import json
-import datetime
-import requests
 import backend
 
 app = Flask(__name__)
 cf_port = os.getenv("PORT")
 
+
 # Default
 @app.route('/')
 def default():
     return '<h1>An error ocurred.</h1>'
+
 
 @app.route('/api', methods=['POST'])
 def bot():
@@ -41,6 +41,7 @@ def bot():
         }
     )
 
+
 @app.route('/assembly', methods=['POST'])
 def get_assembly():
     income_request = json.loads(request.get_data())
@@ -54,11 +55,11 @@ def get_assembly():
             {
                 'type': 'text',
                 'content': {
-                        'Name': assembly['name'],
-                        'Processor': assembly['processor'],
-                        'Memory': assembly['memory'],
-                        'Price': assembly['price'],
-                        'Graphics': assembly['graphics']
+                    'Name': assembly['name'],
+                    'Processor': assembly['processor'],
+                    'Memory': assembly['memory'],
+                    'Price': assembly['price'],
+                    'Graphics': assembly['graphics']
                 }
             }
         ],
@@ -66,10 +67,24 @@ def get_assembly():
             'memory': memory
         }
     )
-    
+
+
+@app.route('/create_order', methods=['POST'])
+def create_order():
+    income_request = json.loads(request.get_data())
+    chosen_assembly = income_request['messages'][0]['content']
+    backend.make_order(chosen_assembly)
+    return jsonify(
+        status=200,
+        replies=[],
+        conversation={
+            'memory': income_request
+        }
+    )
+
 
 if __name__ == '__main__':
-	if cf_port is None:
-		app.run(host='0.0.0.0', port=5000, debug=True)
-	else:
-		app.run(host='0.0.0.0', port=int(cf_port), debug=True)
+    if cf_port is None:
+        app.run(host='0.0.0.0', port=5000, debug=True)
+    else:
+        app.run(host='0.0.0.0', port=int(cf_port), debug=True)
